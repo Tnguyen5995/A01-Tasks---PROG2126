f00076b06a1cb8749eec4df9caf4412a9966313a
thanheuro20057 <thanheuro20057@gmail.com>
2026-02-01 22:28:18 -0500

    from client 1.5

--- PROG2126_A01_TaskTcpPerf/ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
+++ PROG2126_A01_TaskTcpPerf/ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ebbc4909cf177fdf5671d4899bbdcb2766abdbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f814520cb016bb9927f458f2c9a9d52b57ca2d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
