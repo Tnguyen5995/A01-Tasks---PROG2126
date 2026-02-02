b3e7904af1dc1fa5f10c33e520c900ac9010b6a4
tnguyen5995 <tnguyen5995@conestogac.on.ca>
2026-02-01 20:57:23 -0500

    from server 1.0

--- PROG2126_A01_TaskTcpPerf/ClientApp/obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
+++ PROG2126_A01_TaskTcpPerf/ClientApp/obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e499dd3a32386c6d13030fcbfad325655ced3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6965fe3e8f0fc79b788de1fe4d9da0ce123cdb99")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
